54dd458df410fbc53642f69f9d40a096ba34f5fa
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-09-04 07:52:49 +0500

    working on refactoring

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -3,29 +3,33 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-
 st.title("Interactive Data Visualization with Plotly")
-
 
 # Create a selectbox to choose an in-built dataset
 selected_dataset = st.selectbox("Select a Dataset", ["Iris", "Tips", "Wind", "GapMinder"])
 
+# Define a dictionary to map dataset names to corresponding functions
+dataset_functions = {
+    "Iris": px.data.iris,
+    "Tips": px.data.tips,
+    "GapMinder": px.data.gapminder,
+    "Wind": px.data.wind,
+}
 
 # Load the selected dataset
-if selected_dataset == "Iris":
-    df = px.data.iris()
-elif selected_dataset == "Tips":
-    df = px.data.tips()
-elif selected_dataset == "GapMinder":
-    df = px.data.gapminder()
+if selected_dataset in dataset_functions:
+    df = dataset_functions[selected_dataset]()
 else:
-    df = px.data.wind()
+    st.error("Invalid dataset selection. Please choose a valid dataset.")
 
-
+# Display the dataset summary
+st.write(f"## {selected_dataset} Dataset")
 st.write(df)
+
+st.write(f"## {selected_dataset} Columns")
 st.write(df.columns)
 
-# summary stat
+st.write(f"## {selected_dataset} Summary")
 st.write(df.describe())
 
 # Create a sidebar for user input
@@ -38,15 +42,25 @@ color = st.sidebar.selectbox("Color", df.columns)
 size = st.sidebar.selectbox("Size", df.columns)
 hover = st.sidebar.selectbox("Hover Name", df.columns)
 animation = st.sidebar.selectbox("Animation Frame", df.columns)
-animationGrp = st.sidebar.selectbox("Animation Group", df.columns)
+animation_grp = st.sidebar.selectbox("Animation Group", df.columns)
 
+df[size] = pd.to_numeric(df[size], errors="coerce")
 
-# plotting
-fig = px.scatter(df, x= x_axis, y =y_axis, size=size, color=color, hover_name=hover,
-                log_x=True, size_max=55, range_x=[100,100000], range_y=[20,90],
-                animation_frame=animation, animation_group=animationGrp)
-                
+# Plotting
+st.write("### Plot")
+
+fig = px.scatter(df, x=x_axis, y=y_axis, size=size, color=color, hover_name=hover,
+                 log_x=True, size_max=55, range_x=[100, 100000], range_y=[20, 90],
+                 animation_frame=animation, animation_group=animation_grp)
 
 fig.update_layout(width=800, height=600)
 
-st.write(fig)
+st.plotly_chart(fig)
+
+# Add a footer with credits and data source information
+st.markdown(
+    """
+*Data source: [Plotly Express Datasets](https://plotly.com/python-api-reference/generated/plotly.express.data.html)*
+*Built with Streamlit and Plotly*
+"""
+)
